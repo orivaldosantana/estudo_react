a27e8b70bdd5ef4a0f268461c0670e6efbd1d232
Orivaldo Santana <orivaldo@gmail.com>
2019-04-10 23:18:23 -0300

    creating the first react component

---
+++ crash_course/counter-app/src/components/counter.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
+  };
+
+
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}> {this.formatCount()} </span>
+        <button className="btn btn-socondary btn-sm"> Increment </button>
+        <ul> {this.state.tags.map( tag => <li key={tag}> {tag} </li>  )} </ul>
+      </div>
+    );
+  }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
