78f2d097e662e4c1e4cebf8b8bf6b5b8ada80d4b
Orivaldo Santana <orivaldo@gmail.com>
2019-06-16 22:33:39 -0300

    Versão inicial da tela do usuário comum

--- front_end/src/pages/User.js
+++ front_end/src/pages/User.js
@@ -1,9 +1,43 @@
 import React, { Component } from 'react'; 
+import Octicon, {Play} from '@primer/octicons-react'
+import './User.css'; 
 
 class User extends Component { 
     render() {
         return (
-            <h1> User </h1> 
+            <section id="device-list"> 
+                <device>
+                  <header> 
+                    <div className="device-info">     
+                        <span> Porta 1 </span> 
+                        <span className="status"> Conectado - Desligado </span> 
+                    </div>
+                    <Octicon icon={Play} size='medium' ariaLabel='Play'/>
+                  </header>
+                </device> 
+
+                <device>
+                  <header> 
+                    <div className="device-info">     
+                        <span> Porta 2 </span> 
+                        <span className="status"> Conectado - Desligado </span> 
+                    </div>
+                    <a href='https://github.com'>
+                        <Octicon icon={Play} size='medium' ariaLabel='Play'/>
+                    </a>
+                  </header>
+                </device>
+
+                <device>
+                  <header> 
+                    <div className="device-info">     
+                        <span> Porta 3 </span> 
+                        <span className="status"> Conectado - Desligado </span> 
+                    </div>
+                  </header>  
+                </device> 
+ 
+            </section>  
         );
     }
 }
