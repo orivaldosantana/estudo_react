f11b12c97ae8033c93607ba6d314009ef67c8d80
Orivaldo Santana <orivaldo@gmail.com>
2019-06-20 17:24:28 -0300

    Organizando as rotas privadas

--- front_end/src/routes.js
+++ front_end/src/routes.js
@@ -1,15 +1,29 @@
 import React from 'react'; 
-import {Switch, Route} from 'react-router-dom'; 
+import {Switch, Route, Redirect} from 'react-router-dom'; 
 
 import User from './pages/User';
 import Admin from './pages/Admin';
 import Login from './pages/Login'; 
 
+import { isAuthenticated } from './auth'; 
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route {... rest} render={ props => (
+        isAuthenticated() ? (
+            <Component { ... props} />
+        ) : (
+            <Redirect to={{ pathname: '/', state: {form: props.location}}} />
+        )
+    )} />
+
+)
+
+
 function Routes() {
     return (
         <Switch>
-            <Route path="/user" component={User} />
-            <Route path="/admin" component={Admin} />
+            <PrivateRoute path="/user" component={User} />
+            <PrivateRoute path="/admin" component={Admin} />
             <Route path="/login" component={Login} />
             <Route path="/" component={Login} />
         </Switch>    
