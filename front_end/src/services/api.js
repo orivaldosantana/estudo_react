5eb4063544f6ccf571831149362ccbbd0c952a26
Orivaldo Santana <orivaldo@gmail.com>
2019-07-05 15:46:53 -0300

    conexão com a api.orivaiot.tk

--- front_end/src/services/api.js
+++ front_end/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: "http://localhost:3002"
+  baseURL: "http://api.orivaiot.tk"
 });
 
 api.interceptors.request.use(async config => {
