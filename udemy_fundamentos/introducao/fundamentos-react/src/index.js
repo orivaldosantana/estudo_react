df3d2d90bd2e8c1d87b88670fb0eba51461449bd
Orivaldo Santana <orivaldo@gmail.com>
2020-06-06 23:13:45 -0300

    Adicionando CSS

---
+++ udemy_fundamentos/introducao/fundamentos-react/src/index.js
@@ -0,0 +1,12 @@
+
+import './index.css'
+import ReactDOM from 'react-dom';
+import React from 'react'
+
+ReactDOM.render(
+  <div>
+    <strong> Olá Mundo! </strong>
+  </div>
+  ,
+  document.getElementById('root')
+);
